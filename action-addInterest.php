1ea166d348db33c972248d7beac78d3f2b59a3f5
IsmailAabedah <mountazar.94@gmail.com>
2018-07-08 19:57:40 +0430

    Add files via upload

---
+++ action-addInterest.php
@@ -0,0 +1,31 @@
+﻿<?php
+$connection = mysqli_connect("localhost","root","","tft");
+
+if(!$connection)
+  die("connection failed:".mysqli_error());
+mysqli_set_charset($connection,'utf8');
+
+session_start();
+
+$counter=0;
+if(isset($_POST['category']) && !empty($_POST['category']))
+    $counter++;
+
+if($counter==1)
+{
+    $category=$_POST['category'];
+	$result = mysql_query("select interest from Users where id ='".$_SESSION['id']."'");
+	$row = mysql_fetch_array($result);
+    $id = $row['id'];
+
+    $query =  mysql_query("UPDATE Users SET interest ='".$category."' WHERE id = '".$id."'");
+	if(mysqli_query($connection, $query)){
+    echo "Records were updated successfully.";
+} else {
+    echo "ERROR: Could not able to execute $query. " . mysqli_error($connection);
+}
+   
+    $_SESSION['addInterest'] = 'success';
+}
+header('Location: addInterest.php');
+?>
